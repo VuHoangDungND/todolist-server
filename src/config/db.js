f9e056726ac840449ea25b8b0b113be17feaadb6
Dung.ND <94594983+vuhoangdungnd@users.noreply.github.com>
2023-07-21 10:35:53 +0700

    ...

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,17 @@
 // Importing module
 const mysql = require('mysql');
+require('dotenv').config();
 
 // Creating connection
 var db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'todolist',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DBNAME,
 });
 
 db.connect(function (err) {
-    if (err) return res.status(400);
+    if (err) console.log(err);
     console.log('Connected!!!');
 });
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const route = require('./routes');
-const port = process.env.PORT || 5000;
+require('dotenv').config();
+const port = 5000;
 
 app.use(cors());
 app.use(
